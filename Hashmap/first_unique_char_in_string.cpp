6ebb0c491655042924f0884501bf61ece0155669
sandesh362 <sandeshbramahne.362@gmail.com>
2025-11-23 17:04:17 +0530

    first unique char in string using hashmap

---
+++ Hashmap/first_unique_char_in_string.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<unordered_map>
+#include<string>
+using namespace std;
+
+class Solution{
+     public:
+     int firstUniChar(string s){
+        unordered_map<char,int>freq;
+        for(char c:s){    //count frequency of each character
+            freq[c]++;
+
+        }
+        for(int i=0;i<s.length();i++){  //find the first unique character using looping it againn
+            if(freq[s[i]]==1){
+                return i;
+
+            }
+
+        }
+        return -1;   //if there is no unique character
+     }
+};
+
+int main(){
+    Solution sol;
+    string s="leetcode";
+    int index=sol.firstUniChar(s);
+    cout<<"The index of the first unique character is: "<<index<<endl;
+    return 0;
+}
